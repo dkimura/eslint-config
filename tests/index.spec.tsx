041a85525c873e1438ee362f285c828a05233487
Daichi Kimura <dkimura@bdd.xyz>
2021-04-21 18:50:50 +0900

    chore: Rename

--- tests/index.spec.tsx
+++ tests/index.spec.tsx
@@ -4,14 +4,18 @@ import fg from 'fast-glob'
 import { ESLint } from 'eslint'
 
 const cli = new ESLint()
-const target = fg.sync(path.resolve(process.cwd(), './tests/example/**/*.tsx'))
+const target = fg.sync(path.resolve(process.cwd(), './tests/example/**/*'))
 
-const getErrors = (targetPath: string) => cli.lintFiles(targetPath)
+const getErrors = async (targetPath: string) =>
+  (await cli.lintFiles(targetPath)).reduce(
+    (acc, { errorCount }) => acc + errorCount,
+    0
+  )
 
 describe('Validate ESLint configs', () => {
   target.forEach((file) => {
     it(`load config ${file} in ESLint to validate all rules are correct`, async () => {
-      expect(await getErrors(file)).toEqual([])
+      expect(await getErrors(file)).toEqual(0)
     })
   })
 })
