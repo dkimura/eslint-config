051b47d5890f6199f0e90af2c1a432c9a0391ead
Daichi Kimura <dkimura@bdd.xyz>
2021-04-25 16:34:38 +0900

    feat: Include sort rule

--- base.js
+++ base.js
@@ -1,23 +1,28 @@
 const { rules: typeScriptRules } = require('./rules/typescript')
+const { rules: sortRules } = require('./rules/sort')
 
 module.exports = {
   overrides: [
+    // RuleSet for JavaScript
     {
-      // RuleSet for JavaScript
-      files: ['**/*.js', '**/*.jsx'],
       extends: ['prettier'],
+      files: ['**/*.js', '**/*.jsx'],
+      rules: { ...sortRules },
     },
+    // RuleSet for TypeScript
     {
-      // RuleSet for TypeScript
-      files: ['**/*.ts', '**/*.tsx'],
       extends: [
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'prettier',
       ],
+      files: ['**/*.ts', '**/*.tsx'],
+      plugins: ['typescript-sort-keys'],
       rules: {
+        ...sortRules,
         ...typeScriptRules,
       },
     },
   ],
+  plugins: ['simple-import-sort', 'sort-destructure-keys', 'sort-keys-fix'],
 }
